d82d4b011de8ccace8ed769414f8d9db978a140b
Alexander Turchin <AlexanderTurchin1@gmail.com>
2020-03-11 01:21:05 +0300

    Transfered the documentation to interface. Added new method to have possibility add realization for intefaces and abstract classes not generically.

--- Module 2 - C# Fundamentals/IoC/IoC/Container.cs
+++ Module 2 - C# Fundamentals/IoC/IoC/Container.cs
@@ -11,50 +11,36 @@ namespace IoC
     /// <summary>
     /// Represent and IoC container.
     /// </summary>
-    public class Container //: IContainer
+    public class Container : IContainer
     {
         private readonly Dictionary<Type, Type> registeredTypes = new Dictionary<Type, Type>();
         private readonly Dictionary<Type, object> singletonTypes = new Dictionary<Type, object>();
         private readonly Stack<Type> trace = new Stack<Type>();
         
-        /// <summary>
-        /// Register the realization of <typeparamref name="TBase"/> type in <typeparamref name="T"/> type.
-        /// </summary>
-        /// <typeparam name="TBase"></typeparam>
-        /// <typeparam name="T"></typeparam>
         public void Register<TBase, T>()
         where T : TBase
         {
-            Type baseType = typeof(TBase);
-            Type type = typeof(T);
-
-            // if ((baseType.IsClass) 
-            //     && !type.IsSubclassOf(baseType)
-            //     || (baseType.IsInterface) 
-            //     && !type.GetInterfaces().Contains(baseType))
-            // {
-            //     throw new NotInheritException(baseType, type);
-            // }
-            
-            registeredTypes[typeof(TBase)] = typeof(T);
+            Register(typeof(TBase), typeof(T));
         }
 
-        /// <summary>
-        /// Register the type as a singleton.
-        /// </summary>
-        /// <typeparam name="T">The <see cref="Type"/>.</typeparam>
+        public void Register(Type baseType, Type type)
+        {
+             if ((baseType.IsClass) 
+                 && !type.IsSubclassOf(baseType)
+                 || (baseType.IsInterface) 
+                 && !type.GetInterfaces().Contains(baseType))
+             {
+                 throw new NotInheritException(baseType, type);
+             }
+
+             registeredTypes[baseType] = type;
+        }
+
         public void RegisterSingleton<T>()
         {
             singletonTypes[typeof(T)] = GetInstance(typeof(T));
         }
         
-        /// <summary>
-        /// Get an instance of a <paramref name="type"/>.
-        /// </summary>
-        /// <param name="type">The <see cref="Type"/>.</param>
-        /// <returns>Instance of a <paramref name="type"/>.</returns>
-        /// <exception cref="CircularReferenceException">occurs when an instance has a prop of its type and it should be initialized.</exception>
-        /// <exception cref="NotRegisterException">occurs when here is no realization of an interface or abstract class.</exception>
         public object GetInstance(Type type)
         {
             if (singletonTypes.TryGetValue(type, out var singleton))
@@ -86,14 +72,7 @@ namespace IoC
 
             return result;
         }
-        
-        /// <summary>
-        /// Get an instance of a <typeparam name="T"/>.
-        /// </summary>
-        /// <typeparam name="T">The <see cref="Type"/>.</typeparam>
-        /// <returns>Instance of a <typeparam name="T"/>.</returns>
-        /// <exception cref="CircularReferenceException">occurs when an instance has a prop of its type and it should be initialized.</exception>
-        /// <exception cref="NotRegisterException">occurs when here is no realization of an interface or abstract class.</exception>
+
         public T GetInstance<T>()
         {
             return (T) GetInstance(typeof(T));
--- Module 2 - C# Fundamentals/IoC/IoC/Interfaces/IContainer.cs
+++ Module 2 - C# Fundamentals/IoC/IoC/Interfaces/IContainer.cs
@@ -1,22 +1,48 @@
 ﻿using System;
-using System.Reflection;
+using IoC.Exceptions;
 
 namespace IoC.Interfaces
 {
     public interface IContainer
     {
-        void RegisterAssembly(Assembly assembly);
+        /// <summary>
+        /// Register the realization of <typeparamref name="TBase"/> type in <typeparamref name="T"/> type.
+        /// </summary>
+        /// <typeparam name="TBase">The <see cref="Type"/>.</typeparam>
+        /// <typeparam name="T">The <see cref="Type"/>.</typeparam>
+        public void Register<TBase, T>()
+            where T : TBase;
 
-        void Register(Type type);
-
-        void Register<T>();
-
+        /// <summary>
+        /// Register the realization of <paramref name="baseType"/> type in <paramref name="type"/> type.
+        /// </summary>
+        /// <paramref name="baseType">The <see cref="Type"/>.</paramref>
+        /// <paramref name="type">The <see cref="Type"/>.</paramref>
+        /// <exception cref="NotInheritException"> occurs when <paramref name="type"/> is not inherited from <paramref name="baseType"/>.</exception>
         void Register(Type baseType, Type type);
 
-        void Register<TBase, T>();
+        /// <summary>
+        /// Register the type as a singleton.
+        /// </summary>
+        /// <typeparam name="T">The <see cref="Type"/>.</typeparam>
+        public void RegisterSingleton<T>();
 
-        object GetInstance(Type type);
+        /// <summary>
+        /// Get an instance of a <paramref name="type"/>.
+        /// </summary>
+        /// <param name="type">The <see cref="Type"/>.</param>
+        /// <returns>Instance of a <paramref name="type"/>.</returns>
+        /// <exception cref="CircularReferenceException">occurs when an instance has a prop of its type and it should be initialized.</exception>
+        /// <exception cref="NotRegisterException">occurs when here is no realization of an interface or abstract class.</exception>
+        public object GetInstance(Type type);
 
-        T GetInstance<T>();
+        /// <summary>
+        /// Get an instance of a <typeparam name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">The <see cref="Type"/>.</typeparam>
+        /// <returns>Instance of a <typeparam name="T"/>.</returns>
+        /// <exception cref="CircularReferenceException">occurs when an instance has a prop of its type and it should be initialized.</exception>
+        /// <exception cref="NotRegisterException">occurs when here is no realization of an interface or abstract class.</exception>
+        public T GetInstance<T>();
     }
 }
