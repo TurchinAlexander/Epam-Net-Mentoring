39fa0d9d1ebe2c03e830d1052ff23d85ffd1641a
Alexander Turchin <AlexanderTurchin1@gmail.com>
2020-03-22 20:43:19 +0300

    Done the task with Basic Serialization.

---
+++ Module 10 - Serialization/DeSerialization/ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using XmlFormat.Models;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Catalog));
+
+            FileStream stream = new FileStream("books.xml", FileMode.Open);
+            Catalog catalog = xmlSerializer.Deserialize(stream) as Catalog;
+
+            Console.WriteLine("Good");
+        }
+    }
+}
---
+++ Module 10 - Serialization/DeSerialization/XmlFormat/Models/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Security.Permissions;
+using System.Xml.Serialization;
+
+namespace XmlFormat.Models
+{
+    public class Book
+    {
+        [XmlAttribute("id")]
+        public string BookId { get; set; }
+
+        [XmlElement("isbn")]
+        public string Isbn { get; set; }
+
+        [XmlElement("author")]
+        public string Author { get; set; }
+
+        [XmlElement("title")]
+        public string Title { get; set; }
+
+        [XmlElement("genre")]
+        public Genre Genre { get; set; }
+
+        [XmlElement("publisher")]
+        public string Publisher { get; set; }
+
+        [XmlElement("publish_date", DataType = "date")]
+        public DateTime PublisherDate { get; set; }
+
+        [XmlElement("description")]
+        public string Description { get; set; }
+
+        [XmlElement("registration_date", DataType = "date")]
+        public DateTime RegistrationDate { get; set; }
+    }
+}
