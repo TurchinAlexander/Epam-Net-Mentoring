c3648d55e986a5256be3f94e3b80777133a6e88d
Alexander Turchin <AlexanderTurchin1@gmail.com>
2020-03-22 14:32:30 +0300

    Create Redis and Base caching for Fibonacci numbers.

---
+++ Module 9 - Caching/Caching/FibonacciCache/BaseCache.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Caching;
+
+namespace FibonacciCache
+{
+    public class BaseCache : IFibonacciCache
+    {
+        MemoryCache cache = new MemoryCache("Base cache.");
+
+        public void AddItem(int key, int value)
+        {
+            cache.Add(key.ToString(), value.ToString(), null);
+
+        }
+
+        public int GetItem(int key)
+        {
+            object value = cache.Get(key.ToString(), null);
+
+            return Convert.ToInt32(value);
+        }
+
+        public int RemoveItem(int key)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Module 9 - Caching/Caching/FibonacciCache/ICache.cs
@@ -0,0 +1,26 @@
+﻿namespace FibonacciCache
+{
+    public interface IFibonacciCache
+    {
+        /// <summary>
+        /// Add item to the cache.
+        /// </summary>
+        /// <param name="key">Unique identifier of the inserted item.</param>
+        /// <param name="value">The value of the inserted item.</param>
+        void AddItem(int key, int value);
+
+        /// <summary>
+        /// Get the value of the item in the cache by its unique identifier.
+        /// </summary>
+        /// <param name="key">Unique identifier.</param>
+        /// <returns></returns>
+        int GetItem(int key);
+
+        /// <summary>
+        /// Remove the item from the cache.
+        /// </summary>
+        /// <param name="key">Unique identifier of the item.</param>
+        /// <returns></returns>
+        int RemoveItem(int key);
+    }
+}
