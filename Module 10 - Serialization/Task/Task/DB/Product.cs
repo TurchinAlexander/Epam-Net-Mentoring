6aa82a36ede921c939e5ef000aeca86d7d45f496
Alexander Turchin <AlexanderTurchin1@gmail.com>
2020-03-23 23:07:48 +0300

    Add serialization using ISerializable interface.

--- Module 10 - Serialization/Task/Task/DB/Product.cs
+++ Module 10 - Serialization/Task/Task/DB/Product.cs
@@ -1,3 +1,7 @@
+using System.Collections;
+using System.Linq;
+using System.Runtime.Serialization;
+
 namespace Task.DB
 {
     using System;
@@ -6,7 +10,8 @@ namespace Task.DB
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
 
-    public partial class Product
+    [Serializable]
+    public partial class Product : ISerializable
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public Product()
@@ -44,5 +49,78 @@ namespace Task.DB
         public virtual ICollection<Order_Detail> Order_Details { get; set; }
 
         public virtual Supplier Supplier { get; set; }
+        
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            using (var dbContext = new Northwind())
+            {
+                dbContext.Configuration.ProxyCreationEnabled = false;
+
+                if (CategoryID.HasValue)
+                {
+                    var category = dbContext.Categories
+                        .First(c => c.CategoryID == CategoryID);
+
+                    info.AddValue("CategoryID", CategoryID);
+                    info.AddValue("Category", Category);
+                }
+
+                if (SupplierID.HasValue)
+                {
+                    var supplier = dbContext.Suppliers
+                        .First(s => s.SupplierID == SupplierID);
+
+                    info.AddValue("SupplierID", SupplierID);
+                    info.AddValue("Supplier", supplier);
+                }
+                
+                info.AddValue("ProductID", ProductID);
+                info.AddValue("ProductName", ProductName);
+                info.AddValue("QuantityPerUnit", QuantityPerUnit);
+                info.AddValue("UnitPrice", UnitPrice);
+                info.AddValue("UnitsInStock", UnitsInStock);
+                info.AddValue("UnitsOnOrder", UnitsOnOrder);
+                info.AddValue("ReorderLevel", ReorderLevel);
+                info.AddValue("Discontinued", Discontinued);
+
+                var orderDetails = dbContext.Order_Details
+                    .Where(o => o.ProductID == ProductID)
+                    .ToArray();
+
+                info.AddValue("Order_Details", orderDetails);
+            }
+
+            
+        }
+
+        public Product(SerializationInfo info, StreamingContext context)
+        {
+            ProductID = info.GetInt32("ProductID");
+            ProductName = info.GetString("ProductName");
+            QuantityPerUnit = info.GetString("QuantityPerUnit");
+            UnitPrice = (decimal?)info.GetValue("UnitPrice", typeof(decimal?));
+            UnitsInStock = (short?)info.GetValue("UnitsInStock", typeof(short?));
+            UnitsOnOrder = (short?)info.GetValue("UnitsOnOrder", typeof(short?));
+            ReorderLevel = (short?)info.GetValue("ReorderLevel", typeof(short?));
+            Discontinued = info.GetBoolean("Discontinued");
+
+            Order_Details = (ICollection<Order_Detail>)info.GetValue("Order_Details", typeof(ICollection<Order_Detail>));
+            foreach (var orderDetail in Order_Details)
+            {
+                orderDetail.Product = this;
+            }
+
+            CategoryID = (int?)info.GetValue("CategoryID", typeof(int?));
+            if (CategoryID.HasValue)
+            {
+                Category = (Category)info.GetValue("Category", typeof(Category));
+            }
+
+            SupplierID = (int?)info.GetValue("SupplierID", typeof(int?));
+            if (SupplierID.HasValue)
+            {
+                Supplier = (Supplier)info.GetValue("Supplier", typeof(Supplier));
+            }
+        }
     }
 }
